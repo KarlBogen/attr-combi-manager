c5dc5617c879f3a14b561529daa7e51747686813
karlk <karl-klostermann@t-online.de>
2021-01-20 14:46:49 +0100

    Bug VPE Text solved

--- changelog.md
+++ changelog.md
@@ -1,3 +1,4 @@
 UNRELEASED
 
+- Fehler in der Anzeige des VPE-Textes behoben
 - Prüfung Mindestversion 2.0.1, sonst keine Installation
--- new_files/includes/modules/product_combi.php
+++ new_files/includes/modules/product_combi.php
@@ -20,7 +20,7 @@ require_once DIR_FS_DOCUMENT_ROOT . 'vendor-no-composer/karlk/autoload.php';
 	$prod_data = array(	'pid'			=> (int)$product->data['products_id'],
                         'gprice'    	=> isset($products_price) ? $products_price : 0,
         				'tax_id'		=> isset($product->data['products_tax_class_id']) ? $product->data['products_tax_class_id'] : 0,
-                        'products_vpe'	=> isset($product->data['products_vpe']) ? encode_htmlentities($json_vpetext = xtc_get_vpe_name($product->data['products_vpe']) ? $json_vpetext : COMBI_TEXT_PRODUCTS_VPE) : '',
+                        'products_vpe'	=> $json_vpetext = isset($product->data['products_vpe']) ? (xtc_get_vpe_name($product->data['products_vpe']) ? htmlentities(xtc_get_vpe_name($product->data['products_vpe'])) : COMBI_TEXT_PRODUCTS_VPE) : '',
                         'vpe_status'    => isset($product->data['products_vpe_status']) ? $product->data['products_vpe_status'] : 0,
                         'vpe_value'		=> isset($product->data['products_vpe_value']) ? $product->data['products_vpe_value'] : ''
 						);
